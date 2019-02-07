55b45f72bb357582948965c58a7543a3c9766f1f
Rikey Chen <rikey.chen16@gmail.com>
2019-02-06 21:31:59 -0800

    Add functionality to remove and add navbar box shadow on menu display

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 function displayNavbarMenu(e) {
   const navbarMenu = document.getElementsByClassName('navbar-menu')[0];
+  const navbar = document.getElementsByClassName('navbar')[0];
+  console.log(navbar.style);
   if (navbarMenu.className === 'navbar-menu') {
+    navbar.className = 'navbar no-shadow';
     navbarMenu.className = 'navbar-menu open';
     for (let i = 0; i < e.currentTarget.children.length; i++) {
       e.currentTarget.children[i].className = 'hamburger-icon active';
     }
   } else {
+    navbar.className = 'navbar';
     navbarMenu.className = 'navbar-menu';
     for (let i = 0; i < e.currentTarget.children.length; i++) {
       e.currentTarget.children[i].className = 'hamburger-icon';
